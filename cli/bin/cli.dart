804ed49e55c3fec8288077bcd9641ac4621d587e
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2026-06-27 11:26:30 +0700

    Add usage instructions and improve argument handling in CLI

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,9 +1,41 @@
+import 'dart:io'; // Add this line at the top
+
 const version = '0.0.1';
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    print('hello, dwi!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
   } else if (arguments.first == 'version') {
-    print('dartpedia CLI version: $version');
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
   }
 }
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  // If the user didn't pass in arguments, request an article title.
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Otherwise, join the arguments into a single string.
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Current article title: $articleTitle');
+}
+
+
+
+void printUsage() { // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
+}
+
